7deb14fa71ac937a3d75c357c1c61ee275a857ba
Obloev Komronbek <komronbekobloev@gmail.com>
2021-09-12 16:19:10 +0500

    heroku

--- weather/templatetags/weather_tags.py
+++ weather/templatetags/weather_tags.py
@@ -1,7 +1,8 @@
 import json
 from datetime import datetime
 from django import template
-from pytz import timezone as tz
+from pytz import timezone as tz, utc
+from dateutil import parser
 
 register = template.Library()
 
@@ -19,3 +20,14 @@ def tz_time(timezone):
     TZ = tz(timezone)
     date = datetime.now(TZ)
     return date.strftime('%H:%M')
+
+
+@register.filter
+def get_tz_time(date, timezone):
+    dateTime = parser.parse(date)
+    print(dateTime)
+    dateTime = dateTime.replace(tzinfo=utc)
+    timeZone = tz(timezone)
+    tz_time = dateTime.astimezone(timeZone)
+    print(tz_time)
+    return tz_time.strftime('%H:%M')
